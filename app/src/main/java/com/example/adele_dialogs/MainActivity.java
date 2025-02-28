609e13938960b4f5c3d664cb58477aa38d20f5ee
Adele Abramovich <abramovichadele@gmail.com>
2025-02-28 15:08:03 +0200

    first implementation dialog on click listener

--- app/src/main/java/com/example/adele_dialogs/MainActivity.java
+++ app/src/main/java/com/example/adele_dialogs/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.adele_dialogs;
 
+import static android.content.DialogInterface.BUTTON_NEGATIVE;
+import static android.content.DialogInterface.BUTTON_POSITIVE;
+
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btPronounce, btDate, btSave;
     EditText etFirstName, etLastName, etCity;
     private static final String TAG = "MyActivity";
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return insets;
         });
         initElements();
+        context = this;
     }
 
      private void initElements () {
@@ -58,6 +66,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
          }
 
+     }
+
+     private class HandleAlertDialogClickListener implements DialogInterface.OnClickListener {
+
+         @Override
+         public void onClick(DialogInterface dialog, int which) {
+             if (which == BUTTON_NEGATIVE) {
+                 Toast.makeText(context, "cancel", Toast.LENGTH_SHORT).show();
+             }
+             if (which == BUTTON_POSITIVE) {
+                 Toast.makeText(context, "try again", Toast.LENGTH_SHORT).show();
+             }
+         }
      }
 
 //    @Override
